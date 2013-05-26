e69216289b95337183059ced2d27e18c3879622b
cmertz <chris@nimel.de>
2013-05-26 17:28:25 +0200

    initial commit

---
+++ lib/logstash-rails.rb
@@ -0,0 +1,7 @@
+require "logstash-rails/version"
+
+module Logstash
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/logstash-rails/version.rb
@@ -0,0 +1,5 @@
+module Logstash
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
