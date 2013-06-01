ef4d4a5b53faad80635098d6e1830ef374dca6f0
cmertz <chris@nimel.de>
2013-06-01 21:22:39 +0200

    use class << self

--- lib/logstash-rails.rb
+++ lib/logstash-rails.rb
@@ -2,7 +2,11 @@ require 'logstash-rails/formatter'
 require 'logstash-rails/redis'
 
 module LogstashRails
-  def self.push(*args)
-    Redis.push(Formatter.format(*args))
+  class << self
+
+    def push(*args)
+      Redis.push(Formatter.format(*args))
+    end
+
   end
 end
