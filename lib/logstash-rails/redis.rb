d1a3977639e8fc4a772883587071d9e4c56927e0
cmertz <chris@nimel.de>
2013-06-01 21:19:31 +0200

    use correct accessor

--- lib/logstash-rails/redis.rb
+++ lib/logstash-rails/redis.rb
@@ -1,7 +1,7 @@
 module LogstashRails
   class Redis < Struct.new(:redis_connection, :key)
     def push(msg)
-      raise unless @redis.rpush(@key, msg)
+      raise unless redis_connection.rpush(key, msg)
     end
   end
 end
