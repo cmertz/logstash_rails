8e32ff684a3b067cc9d16073ff45e36c81b0ebf4
cmertz <chris@nimel.de>
2013-06-01 12:18:54 +0200

    remove 'test' switch

--- lib/logstash-rails.rb
+++ lib/logstash-rails.rb
@@ -3,15 +3,6 @@ require 'logstash-rails/redis'
 
 module LogstashRails
 
-  def self.test=(value)
-    raise ArgumentError unless value == true || value == false
-    @test = value
-  end
-
-  def self.test?
-    @test
-  end
-
   def self.push(*args)
     Redis.push(Formatter.format(*args))
   end
