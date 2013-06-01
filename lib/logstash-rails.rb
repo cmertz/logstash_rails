fd1856417a48b70616d3dd5ef1038c0fcbbeae03
cmertz <chris@nimel.de>
2013-06-01 21:24:29 +0200

    handle subscriptions and unsubscriptions in LogstashRails

--- lib/logstash-rails.rb
+++ lib/logstash-rails.rb
@@ -4,6 +4,18 @@ require 'logstash-rails/redis'
 module LogstashRails
   class << self
 
+    def subscribe(event_type)
+      @subscriptions ||= {}
+
+      @subscriptions[event_type] = ActiveSupport::Notifications.subscribe(event_type) do |*args|
+        push(*args)
+      end
+    end
+
+    def unsubscribe(event_type)
+      ActiveSupport::Notifications.unsubscribe(@subscriptions[event_type])
+    end
+
     def push(*args)
       Redis.push(Formatter.format(*args))
     end
