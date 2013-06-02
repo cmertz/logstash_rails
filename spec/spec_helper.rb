da768a997c382151aaf7d977797be2699f2dceca
cmertz <chris@nimel.de>
2013-06-02 18:40:13 +0200

    fix coverage reports

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,16 @@
+require 'coveralls'
+require 'simplecov'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  add_filter('spec')
+end
+
 require 'bundler'
 Bundler.require :default, :development, :test
-
-require 'coveralls'
-Coveralls.wear!
 
 require 'logstash_rails'
