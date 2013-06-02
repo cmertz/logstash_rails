81743dbfa2333e6cba0775c374db5fc3a1d4cd19
cmertz <chris@nimel.de>
2013-06-02 18:07:02 +0200

    require gem groups in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
+Bundler.require :default, :development, :test
+
 require 'coveralls'
 Coveralls.wear!
 
-require 'minitest'
-require 'minitest/autorun'
 require 'logstash-rails'
-require 'fakeredis'
