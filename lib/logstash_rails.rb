7c5008fe21bfb6645152acbc8fab0a3d4967d753
cmertz <chris@nimel.de>
2013-06-07 20:22:31 +0200

    remove LogstashRails.unsubscribe

--- lib/logstash_rails.rb
+++ lib/logstash_rails.rb
@@ -2,63 +2,54 @@ require 'logstash_rails/formatter'
 require 'active_support'
 
 module LogstashRails
-  class << self
+  extend self
 
-    # configures LogstashRails
-    #
-    # @param redis
-    # @param key [String]
-    # @param handle_all [Boolean]
-    #
-    def config(redis, key = 'logstash', handle_all = true)
-      unless handle_all.is_a?(TrueClass) || handle_all.is_a?(FalseClass)
-        raise ArgumentError
-      end
-
-      raise ArgumentError unless key.is_a?(String)
-
-      raise ArgumentError unless redis
-
-      @redis = redis
-      @key   = key
-
-      subscribe(/.*/) if handle_all
+  # configures LogstashRails
+  #
+  # @param redis
+  # @param key [String]
+  # @param handle_all [Boolean]
+  #
+  def config(redis, key = 'logstash', handle_all = true)
+    unless handle_all.is_a?(TrueClass) || handle_all.is_a?(FalseClass)
+      raise ArgumentError
     end
 
-    # subscribes LogstashRails to an ActiveSupport notification
-    #
-    # @param event_type [String|Regexp]
-    #
-    def subscribe(event_type)
-      @subscriptions ||= {}
+    raise ArgumentError unless key.is_a?(String)
 
-      @subscriptions[event_type] = ActiveSupport::Notifications.subscribe(event_type) do |*args|
-        push(*args)
-      end
-    end
+    raise ArgumentError unless redis
 
-    # unsubscribe LogstashRails form an ActiveSupport notification
-    #
-    # @param event_type [String|Regexp] this must be the equal to the event_type that was subscribed with
-    #
-    def unsubscribe(event_type)
-      ActiveSupport::Notifications.unsubscribe(@subscriptions[event_type])
-    end
+    @redis = redis
+    @key   = key
 
-    private
-
-    def push(event_type, *args)
-      begin
-        @redis.rpush(@key, Formatter.format(event_type, *args))
-      rescue
-        log $!
-      end
-    end
-
-    def log(exception)
-      msg = exception.message + "\n " + exception.backtrace.join("\n ")
-      Rails.logger.error(msg)
-    end
-
+    subscribe(/.*/) if handle_all
   end
+
+  # subscribes LogstashRails to an ActiveSupport notification
+  #
+  # @param event_type [String|Regexp]
+  #
+  def subscribe(event_type)
+    @subscriptions ||= {}
+
+    @subscriptions[event_type] = ActiveSupport::Notifications.subscribe(event_type) do |*args|
+      push(*args)
+    end
+  end
+
+  private
+
+  def push(event_type, *args)
+    begin
+      @redis.rpush(@key, Formatter.format(event_type, *args))
+    rescue
+      log $!
+    end
+  end
+
+  def log(exception)
+    msg = exception.message + "\n " + exception.backtrace.join("\n ")
+    Rails.logger.error(msg)
+  end
+
 end
--- spec/subscription_spec.rb
+++ spec/subscription_spec.rb
@@ -35,16 +35,6 @@ describe LogstashRails do
     redis.lpop('logstash').wont_be_nil
   end
 
-  it 'can be unsubscribed from events' do
-    LogstashRails.config(redis, 'logtstash', false)
-    LogstashRails.subscribe("process_action.action_controller")
-    LogstashRails.unsubscribe("process_action.action_controller")
-
-    ActiveSupport::Notifications.instrument("process_action.action_controller")
-
-    redis.lpop('logstash').must_be_nil
-  end
-
   it 'uses the provided redis key' do
     LogstashRails.config(redis, 'foorediskey', false)
     LogstashRails.subscribe("process_action.action_controller")
