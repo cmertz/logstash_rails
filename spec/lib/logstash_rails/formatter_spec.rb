26bdbff7275cd9b6047174ae4a2f9cc7042aa9b9
cmertz <chris@nimel.de>
2013-06-07 22:34:40 +0200

    add spec for event format

---
+++ spec/lib/logstash_rails/formatter_spec.rb
@@ -0,0 +1,17 @@
+describe 'generated logstash event' do
+
+  before do
+    Redis::Connection::Memory.reset_all_databases
+    LogstashRails.config(transport: :redis, redis: Redis.new, redis_key: 'logstash')
+    ActiveSupport::Notifications.instrument("process_action.action_controller")
+  end
+
+  it 'is parsebale json' do
+    JSON.parse(Redis.new.lpop('logstash'))
+  end
+
+  it 'has the event type as message' do
+    JSON.parse(Redis.new.lpop('logstash'))['@message'].should eq "process_action.action_controller"
+  end
+
+end
