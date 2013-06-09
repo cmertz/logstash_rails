48bd9c9f9b3275fce168aadd9fab6396f5e2ad0a
cmertz <chris@nimel.de>
2013-06-09 12:56:41 +0200

    proper spec setup

--- spec/lib/logstash_rails/transport/logstash_tcp_spec.rb
+++ spec/lib/logstash_rails/transport/logstash_tcp_spec.rb
@@ -1,7 +1,31 @@
 describe LogstashRails::Transport::LogstashTcp do
-  subject do
-    LogstashRails::Transport::LogstashTcp.new(port: 10000)
+
+  before do
+    @server = TCPServer.new(9000)
+
+    @thread = Thread.new do
+      client = @server.accept
+      @received = client.gets
+      client.close
+    end
   end
 
-  it { should respond_to :push }
+  after do
+    @server.close
+  end
+
+  let :logstash_tcp do
+    LogstashRails::Transport::LogstashTcp.new(port: 9000)
+  end
+
+  it do
+    logstash_tcp.should respond_to :push
+  end
+
+  it 'sends data over tcp' do
+    logstash_tcp.push 'toto'
+    logstash_tcp.destroy
+    @thread.join
+    @received.should eq 'toto'
+  end
 end
