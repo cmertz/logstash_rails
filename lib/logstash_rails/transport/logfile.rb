01f73bbc409f18e7726fc4b6ef5f4eae44045781
cmertz <chris@nimel.de>
2013-06-15 18:50:53 +0200

    add logfile transport

---
+++ lib/logstash_rails/transport/logfile.rb
@@ -0,0 +1,24 @@
+require 'logger'
+
+module LogstashRails
+  module Transport
+    class Logfile < TransportBase
+
+      def initialize(options = {})
+        logfile  = options[:logfile] || 'log/logstash_rails.log'
+
+        @logger = ::Logger.new(logfile)
+
+        @logger.formatter = lambda do |severity, datetime, progname, msg|
+          "#{msg}\n"
+        end
+        @logger.level = Logger::INFO
+      end
+
+      def push(json_event)
+        @logger.error(json_event)
+      end
+
+    end
+  end
+end
---
+++ spec/lib/logstash_rails/transport/logfile_spec.rb
@@ -0,0 +1,26 @@
+describe LogstashRails::Transport::Logfile do
+
+  let :logfile do
+    Tempfile.new(__FILE__)
+  end
+
+  after do
+    logfile.unlink
+  end
+
+  subject do
+    LogstashRails::Transport::Logfile.new(logfile: logfile)
+  end
+
+  it { should respond_to :push }
+
+  it 'writes events to a logfile' do
+    event = 'foobar_event'
+
+    subject.push(event)
+
+    logfile.rewind
+    logfile.read.should include(event)
+  end
+
+end
