644da97b75e88e7d87d30a03478f831ab4d84934
cmertz <chris@nimel.de>
2013-10-07 13:59:31 +0200

    add specs

--- spec/lib/logstash_rails/formatter_spec.rb
+++ spec/lib/logstash_rails/formatter_spec.rb
@@ -1,9 +1,33 @@
 describe LogstashRails::Formatter do
 
-  it 'flattens params'
+  subject do
+    lambda do |payload|
+      LogstashRails::Formatter.new.format('event', Time.now, Time.now, 1, payload)
+    end
+  end
 
-  it 'does not flatten params'
+  it 'flattens params' do
+    formatter = LogstashRails::Formatter.new(flatten_params: true)
+    payload = {params:{a: {b: 1}, c: 2}}
 
-  it 'deletes Rack::Request'
+    result = formatter.format('event', Time.now, Time.now, 1, payload)
+
+    JSON.parse(result).should include({'params' => {'a.b' => 1, 'c' => 2}})
+  end
+
+  it 'does not flatten params' do
+    formatter = LogstashRails::Formatter.new
+    payload = {params:{a: {b: 1}, c: 2}}
+
+    result = formatter.format('event', Time.now, Time.now, 1, payload)
+
+    JSON.parse(result).should include({'params' => {'a' => {'b' => 1}, 'c' => 2}})
+  end
+
+  it 'deletes Rack::Request' do
+    payload = {request: 'toto'}
+
+    subject.call(payload).should_not include('request')
+  end
 
 end
