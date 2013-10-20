9d018dce5b161edb0494f0bf6ad0a7f246391405
cmertz <chris@nimel.de>
2013-10-20 03:16:26 +0200

    move specs

--- spec/lib/logstash_rails/formatter/basic_spec.rb
+++ spec/lib/logstash_rails/formatter/basic_spec.rb
@@ -1,17 +1,21 @@
 describe LogstashRails::Formatter::Basic do
 
   subject do
-      LogstashRails::Formatter.get.perform('event', Time.now, Time.now, 1, {request: 'toto'})
+    LogstashRails::Formatter.get.perform('event', Time.now, Time.now, 1, {request: 'toto'})
   end
 
   it 'deletes Rack::Request' do
     subject.should_not include('request')
   end
 
-  [:process_id, :host, :message].each do |field|
+  %w(message host process_id source).each do |field|
     it "adds #{field}" do
-      subject.should include(field.to_s)
+      subject.should include(field)
     end
+  end
+
+  it 'has the event type as message' do
+    JSON.parse(subject)["message"].should eq 'event'
   end
 
 end
